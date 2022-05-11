8923e0791b7ebf33c837fa74ce6cab065e5c8340
Darion Spaargaren <djspaargaren@outlook.com>
2022-05-11 08:09:02 +0200

    Remove unnecessary comment, change project to use non-mobile Vulkan API

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -27,7 +27,6 @@ public partial class PlayerController : RigidDynamicBody3D
 
 	private float defaultGravityScale;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
